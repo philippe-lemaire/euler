082f3b39e7c431716af8a1f865ed83f811ec70b9
Philippe Lemaire <lemaire.philippe@gmail.com>
2016-04-02 22:27:43 +0200

    problem 32 solved

--- 32-pandigital-products.py
+++ 32-pandigital-products.py
@@ -9,3 +9,24 @@ Find the sum of all products whose multiplicand/multiplier/product identity can 
 
 HINT: Some products can be obtained in more than one way so be sure to only include it once in your sum.
 '''
+import itertools
+
+def is_pandigital(x,y):
+    my_list = []
+    for digit in  str(x) + str(y) + str(x * y):
+        my_list.append(digit)
+    my_list.sort()
+    return "".join(my_list) == "123456789"
+
+products_list = []
+
+
+for multiplicand in range(2, 2000):
+    for multiplier in range(2000, 2 , -1):
+        if is_pandigital(multiplicand, multiplier):
+            products_list.append(multiplicand * multiplier)
+            
+
+products_list = set(products_list)
+
+print(sum(products_list))
