6bfa4613f207b7c36a0e219a94fea571ee918645
Philippe Lemaire <lemaire.philippe@gmail.com>
2016-04-09 07:20:32 +0200

    problem 35 solved

--- 35-circular-primes.py
+++ 35-circular-primes.py
@@ -28,6 +28,14 @@ def is_a_prime(n):
         i = i + 2
     return True
 
+def rotate(num):
+    '''takes an integer and retuns a set of all its possible digit rotations'''
+    num_string = str(num)
+    rotations_list = []
+    for i in range(0, len(num_string)):
+        rotations_list.append(int(num_string[1 +i:] + num_string[0:1 + i]))
+    return set(rotations_list)
+
 primes_bom = []
 
 for num in range(3, 1000000, 2):
@@ -41,18 +49,18 @@ for prime in primes_bom:
     test = str(prime)
     if ("0" in test) or ("2" in test) or ("4" in test) or ("6" in test) or ("8" in test):
         primes_trimmed.remove(prime)
-        print(prime, " removed because at least one permutation would be even.")
+        print(prime, " removed because at least one rotation would be even.")
         
 primes_trimmed.insert(0,2)
 
 final_list = list(primes_trimmed)
 
 for prime in primes_trimmed:
-    perms = permutations(str(prime))
-    for perm in perms:
-        if not is_a_prime(int("".join(perm))):
+    rotations = rotate(prime)
+    for rotation in rotations:
+        if not is_a_prime(rotation):
             final_list.remove(prime)
-            print(prime, " removed because at least one permutation isn’t prime.")
+            print(prime, " removed because at least one rotation isn’t prime.")
             break
         
 print(final_list)
