8973294982b7ca8e4bdf8a05cd6ce52ef44e6c73
Philippe Lemaire <lemaire.philippe@gmail.com>
2019-09-09 15:11:47 +0200

    fix line length issues

--- 27-quadratic-primes.py
+++ 27-quadratic-primes.py
@@ -6,9 +6,14 @@ Euler discovered the remarkable quadratic formula:
 
 n² + n + 41
 
-It turns out that the formula will produce 40 primes for the consecutive values n = 0 to 39. However, when n = 40, 402 + 40 + 41 = 40(40 + 1) + 41 is divisible by 41, and certainly when n = 41, 41² + 41 + 41 is clearly divisible by 41.
+It turns out that the formula will produce 40 primes for the consecutive values n = 0 to 39. 
+However, when n = 40, 402 + 40 + 41 = 40(40 + 1) + 41 is divisible by 41, 
+and certainly when n = 41, 41² + 41 + 41 is clearly divisible by 41.
 
-The incredible formula  n² − 79n + 1601 was discovered, which produces 80 primes for the consecutive values n = 0 to 79. The product of the coefficients, −79 and 1601, is −126479.
+The incredible formula  n² − 79n + 1601 was discovered, which produces 80 primes 
+for the consecutive values n = 0 to 79. 
+
+The product of the coefficients, −79 and 1601, is −126479.
 
 Considering quadratics of the form:
 
@@ -16,11 +21,14 @@ n² + an + b, where |a| < 1000 and |b| < 1000
 
 where |n| is the modulus/absolute value of n
 e.g. |11| = 11 and |−4| = 4
-Find the product of the coefficients, a and b, for the quadratic expression that produces the maximum number of primes for consecutive values of n, starting with n = 0.
+Find the product of the coefficients, a and b, for the quadratic expression 
+that produces the maximum number of primes for consecutive values of n, starting with n = 0.
 '''
 
+
 def is_a_prime(n):
-    ''' takes a positive integer as argument. Returns True if it's a prime number, False if it's not a prime'''
+    ''' takes a positive integer as argument. 
+        Returns True if it's a prime number, False if it's not a prime'''
     n = abs(int(n))
     if n < 2:
         return False
@@ -60,4 +68,3 @@ for a in range(-999, 1000):
 
 print("n² +" , best_coefs[0],"n +" , best_coefs[1],"donne", best, "nombres premiers consécutifs")
 print("le dernier mot = ", best_coefs[0] * best_coefs[1])
-
