b1c06e697c1500f913a7b2d28f0c33d3f61c7a3d
Philippe Lemaire <lemaire.philippe@gmail.com>
2019-09-13 16:48:16 +0200

    solved prob 28

--- 28-number-spiral-diagonals.py
+++ 28-number-spiral-diagonals.py
@@ -16,4 +16,50 @@ It can be verified that the sum of the numbers on the diagonals is 101.
 What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed in the same way?
 '''
 
-import numpy
+
+NORTH, S, W, E = (0, -1), (0, 1), (-1, 0), (1, 0) # directions
+turn_right = {NORTH: E, E: S, S: W, W: NORTH} # old -> new direction
+
+def spiral(width, height):
+    if width < 1 or height < 1:
+        raise ValueError
+    x, y = width // 2, height // 2 # start near the center
+    dx, dy = NORTH # initial direction
+    matrix = [[None] * width for _ in range(height)]
+    count = 0
+    while True:
+        count += 1
+        matrix[y][x] = count # visit
+        # try to turn right
+        new_dx, new_dy = turn_right[dx,dy]
+        new_x, new_y = x + new_dx, y + new_dy
+        if (0 <= new_x < width and 0 <= new_y < height and
+            matrix[new_y][new_x] is None): # can turn right
+            x, y = new_x, new_y
+            dx, dy = new_dx, new_dy
+        else: # try to move straight
+            x, y = x + dx, y + dy
+            if not (0 <= x < width and 0 <= y < height):
+                return matrix # nowhere to go
+
+
+
+def print_matrix(matrix):
+    width = len(str(max(el for row in matrix for el in row if el is not None)))
+    fmt = "{:0%dd}" % width
+    for row in matrix:
+        print(" ".join("_"*width if el is None else fmt.format(el) for el in row))
+
+spiral1001 = spiral(1001, 1001)
+
+# count first diagonal total
+NW_SE_diagonal = 0
+NE_SW_diagonal = 0
+for i in range(1001):
+    NW_SE_diagonal += spiral1001[i][i]
+    NE_SW_diagonal += spiral1001[i][1000 - i]
+
+print("NW_SE_diagonal = ", NW_SE_diagonal)
+print("NE_SW_diagonal = ", NE_SW_diagonal)
+print("total = ", NW_SE_diagonal + NE_SW_diagonal - 1) #remove the center cell counted twice
+
